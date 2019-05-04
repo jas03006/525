9ca403bab26b27d85c8491e9257c118acae36bf9
MoonlightRiver <baedro1@naver.com>
2019-05-04 19:54:40 +0900

    Error fixed

--- Life_Chart/life_chart.js
+++ Life_Chart/life_chart.js
@@ -123,7 +123,7 @@ function confirm(){
   // 뒤로가기 누르면 다시 앞페이지로 이동
   window.history.forward(1);
   // 기존 페이지를 새로운 페이지로 변경
-  location.replace("https://jas03006.github.io/525/Building_Stroy/Building_Story.html");
+  location.replace("https://jas03006.github.io/525/Building_Story/Building_Story.html");
 }
 initialize();
 
