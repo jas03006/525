f1e562fa74e8b114065ee31893c21609bea22f31
MoonlightRiver <baedro1@naver.com>
2019-05-04 14:26:48 +0900

    Add Building Story v1

---
+++ Building_Story/Building_Story.js
@@ -0,0 +1,129 @@
+var questionNum = 1;
+
+function initialize(){
+  
+  addQuestionBox();
+}
+
+function deleteAll(){
+  for(var i = 0; i < questionNum; i++){
+    var qDivId = "qDiv" + (i + 1);
+    document.getElementById(qDivId).remove();
+  }
+  document.getElementById("myButton").remove();
+  document.getElementById("confirmDiv").remove();
+  questionNum = 1;
+  
+  initialize();
+}
+
+function addQuestionBox(){
+  var qDiv = document.createElement("div");
+  var mDiv = document.createElement("div");
+  var cDiv = document.createElement("div");
+  var memories = document.createElement("textarea");
+  var comments = document.createElement("textarea");
+  var buttonDiv = document.createElement("div");
+  var addButton = document.createElement("a");
+  var addQuestionText = document.createElement("div");
+  var confirmDiv = document.createElement("div");
+  var confirmButton = document.createElement("a");
+  
+  //Question box specification
+  //Text area specification
+  memories.className = "questionBox";
+  memories.id = "question1_" + questionNum;
+  memories.placeholder = "Write question #" +  questionNum + " here";
+
+  comments.className = "questionBox";
+  comments.id = "question2_" + questionNum;
+  comments.placeholder = "Write Comment #" +  questionNum + " here";
+  
+  //Enter listener
+  //memories.addEventListener('keyup', enterEvent);
+  //comments.addEventListener('keyup', enterEvent);
+  qDiv.innerHTML = "<h2>Q" + questionNum + ". </h2><h3>Sample Question</h3>";
+  mDiv.innerHTML = "Memories<br />"
+  cDiv.innerHTML = "Comments<br />"
+  mDiv.appendChild(memories);
+  cDiv.appendChild(comments);
+  qDiv.appendChild(mDiv);
+  qDiv.appendChild(cDiv);
+  qDiv.id = "qDiv" + questionNum;
+  mDiv.id = "mDiv" + questionNum;
+  cDiv.id = "cDiv" + questionNum;
+  qDiv.className = "left";
+  mDiv.className = "center";
+  cDiv.className = "center";
+  
+  //Add question to the content
+  document.getElementById("paper").appendChild(qDiv);
+  
+  //Add question button specification
+  addButton.innerHTML = "+";
+  addButton.className = "addButton";
+  
+  addButton.addEventListener("click", function (e){
+    submit(comments);
+  });
+  
+  addQuestionText.innerHTML = "Add Question";
+  addQuestionText.style.color = "#3ea99f";
+  
+  buttonDiv.className = "center";
+  buttonDiv.appendChild(addButton);
+  buttonDiv.appendChild(addQuestionText);
+  buttonDiv.id = "myButton";
+  
+  confirmButton.id = "confirmButton";
+  confirmButton.innerHTML = "Confirm";
+  confirmButton.addEventListener("click", confirm);
+  confirmDiv.appendChild(confirmButton);
+  confirmDiv.id = "confirmDiv";
+  
+  //Add button to the content
+  document.getElementById("paper").appendChild(buttonDiv);
+  document.getElementById("paper").appendChild(confirmDiv);
+  
+  return comments;
+}
+
+function confirm(){
+  for(var i = 0; i < questionNum; i++){
+    var question1Id = "question1_" + (i + 1);
+    console.log(document.getElementById(question1Id).value);
+    var question2Id = "question2_" + (i + 1);
+    console.log(document.getElementById(question2Id).value);
+  }
+  deleteAll();
+}
+
+function enterEvent(e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      // code for enter
+      this.value = this.value.replace(/(\r\n|\n|\r)/gm, "");
+      if(this.value != ""){
+        submit(this);
+        return;
+      }
+    }
+}
+
+function submit(questionBox){
+  var question = questionBox.value;
+  
+  //Remove enter listener
+  questionBox.removeEventListener('keyup', enterEvent);
+
+
+  //Remove add button
+  document.getElementById("myButton").remove();
+  document.getElementById("confirmDiv").remove();
+  questionNum++;
+
+  //Add new questionBox
+  addQuestionBox().focus();
+}
+
+initialize();
