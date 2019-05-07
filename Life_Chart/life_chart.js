14d70af08d89fa903f06116211f9a2e073baf2a5
unknown <wjdgns5104@kaist.ac.kr>
2019-05-08 02:34:40 +0900

    life-chart v1.0

--- Life_Chart/life_chart.js
+++ Life_Chart/life_chart.js
@@ -1,6 +1,19 @@
-years = [2016, 2017, 2018, 2019]
-pins = [{year: 2017, month: 6, date: 30}, {year: 2019, month: 12, date: 3}]
-memos = [{text: "Hi", year: 2017, month: 6, date: 30}, {text: "I'm Jeonghun", year: 2019, month: 12, date: 1}]
+var yearStart = 2016;
+var currYear = 2019;
+var currPin = -1;
+var framePinName = -1;
+
+memos = [{title: "SS internship", year: 2016, month: 1, date: 1, comment: "Hello"}, {title: "AI conference", year: 2017, month: 12, date: 1, comment: ""}, {title: "HCI team project", year: 2019, month: 12, date: 31, comment: ""}]
+
+function findMemo(memoTitle){
+  for(var i = 0; i < memos.length; i++){
+    if(memos[i].title == memoTitle){
+      return memos[i];
+    }
+  }
+  //Fail to find such title
+  return -1;
+}
 
 function dateFraction(year, month, date){
   var date_in_month = [28, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -27,89 +40,263 @@ function dateFraction(year, month, date){
   
   return date_sum / full_date;
 }
-
-function initialize(){
+function drawYear(){
   var chart = document.getElementById("life-chart");
   
   //Draw years
-  for(var i = 0; i < years.length; i++){
+  for(var i = 0; i < 4; i++){
     var newYear = document.createElement("div");
     
-    newYear.innerHTML = years[i];
-    newYear.className = "chart" + (i + 1);
-    chart.appendChild(newYear);
-  }
-  //Fake for pins
-  for(var i = 0; i < years.length; i++){
-    var newYear = document.createElement("div");
+    newYear.id = yearStart + i;
+    newYear.className = "col-md-3 year";
+    newYear.innerHTML = yearStart + i;
     
-    newYear.id = years[i];
-    newYear.className = "fake";
+    if((yearStart + i) % 2 == 0){
+      newYear.style.backgroundColor = "#FFFFFF";
+    }else{
+      newYear.style.backgroundColor = "#AAAAAA";
+    }
+    //newYearDiv.appendChild(newYear);
+    //newYear.style.top = "-150px";
     chart.appendChild(newYear);
   }
+}
+
+function initialize(){
+  drawYear();
+  var pinNum = memos.length;
   
   //Draw pins and memos
-  for(var i = 0; i < memos.length; i++){
-      var currYear = document.getElementById(memos[i].year);
+  for(var i = 0; i < pinNum; i++){
+    var currYear = document.getElementById(memos[i].year);
+    var canvas = document.getElementById("life-chart");
     
-      drawPin(memos[i], currYear, currYear.offsetLeft, currYear.offsetTop, currYear.offsetWidth);
-      drawMemo(memos[i]);
+    //console.log(memos[i], relLeft, relTop);
+    if(memos[i].year >= yearStart && memos[i].year < (yearStart + 4)){
+      var relLeft = currYear.offsetLeft - canvas.offsetLeft;
+      var relTop = currYear.offsetTop - canvas.offsetTop;
+      drawPin(memos[i], currYear, relLeft, relTop, currYear.offsetWidth);
+    }
+    //drawMemo(memos[i]);
   }
   
 }
-  //console.log(document.getElementById(years[i]).offsetLeft, document.getElementById(years[i]).offsetWidth);
  
 function drawPin(pins, currYear, left, top, width){
-  var pin = document.createElement("div");
+  var pinDiv = document.createElement("div");
+  var pin = document.createElement("span");
+  var memo = document.createElement("span");
+  var canvas = document.getElementById("life-chart");
   var frac = Math.round(dateFraction(pins.year, pins.month, pins.date) * 100) / 100;
   
-  pin.className = "fas fa-thumbtack";
-  pin.id = pins.year.toString() + '.' + pins.month.toString() + '.' + pins.date.toString();
-  pin.style.left = (frac * 100 - 57).toString() + "%";
-  pin.style.top = "-30px";
-  pin.addEventListener("click", turnMemo);
-  /*pin.addEventListener("onmouseover", function(e){
-    pin.style.fontSize = 24px;
-  });*/
-  currYear.appendChild(pin);
+  pin.className = "fas fa-pen";
+  //pin.id = pins.year.toString() + '.' + pins.month.toString() + '.' + pins.date.toString();
+  pin.id = pins.title;
+  pinDiv.className = "pinDiv";
+  
+  pinDiv.style.left = (100 + (frac - 0.5) * width).toString() + "px";
+  pinDiv.style.top = "-200%";
+  //pinDiv.style.left = (left + frac * width - 6 - i * 200).toString() + "px";
+  //pinDiv.style.left = (-14 + frac * 114).toString() + "%";
+  //pinDiv.style.top = (top - 80).toString() + "px";
+  pin.addEventListener("click", pinPick);
+  pin.addEventListener("mouseover", pinIn);
+  pin.addEventListener("mouseout", pinOut);
+  memo.innerHTML = pins.title;
+  memo.className = "memosOff";
+  
+  
+  pinDiv.appendChild(pin);
+  pinDiv.appendChild(memo);
+  currYear.appendChild(pinDiv);
+  //pinDiv.appendChild(pin);
 }
 
-//Absolute
-// function drawPin(pins, currYear, left, top, width){
-//   var pin = document.createElement("div");
-//   var frac = Math.round(dateFraction(pins.year, pins.month, pins.date) * 100) / 100;
+function pinIn(){
+  var memoClass = this.parentElement.children[1].className;
+  var commentDiv = document.getElementById("commentDiv");
+  var comment = document.getElementById("comment");
   
-//   pin.className = "fas fa-thumbtack";
-//   pin.id = pins.year.toString() + '.' + pins.month.toString() + '.' + pins.date.toString();
-//   pin.style.left = Math.round(left + (width * frac)).toString() + "px";
-//   pin.style.top = (top - 10).toString() + "px";
-//   pin.addEventListener("click", turnMemo);
-//   currYear.appendChild(pin);
-// }
-
-function drawMemo(memo){
-  var currPin = document.getElementById(memo.year.toString() + '.' + memo.month.toString() + '.' + memo.date.toString());
-  var memoDiv = document.createElement("div");
-  
-  memoDiv.innerHTML = memo.text;
-  memoDiv.className = "memosOn";
-  currPin.appendChild(memoDiv);
-}
-
-function turnMemo(){
-  if(this.children[0].className == "memosOn"){
-    this.children[0].className = "memosOff";
-  }else{
-    this.children[0].className = "memosOn";
+  if(memoClass == "memosOff"){
+    this.parentElement.children[1].className = "memosOn";
+    if(currPin == -1){
+      comment.value = findMemo(this.parentElement.children[1].innerHTML).comment;
+    }
+    commentDiv.style.visibility = "visible";
   }
+  if(memoClass == "fixedOut"){
+    this.parentElement.children[1].className = "fixedOn";
+  }
+}
+
+function pinOut(){
+  var memoClass = this.parentElement.children[1].className;
+  var commentDiv = document.getElementById("commentDiv");
+
+  if(memoClass == "memosOn"){
+    this.parentElement.children[1].className = "memosOff";
+    if(currPin == -1){
+      commentDiv.style.visibility = "hidden";
+    }
+  }
+  if(memoClass == "fixedOn"){
+    this.parentElement.children[1].className = "fixedOut";
+    commentDiv.style.visibility = "visible";
+  }
+}
+
+function pinPick(){
+  var memoClass = this.parentElement.children[1].className;
+  
+  if(memoClass == "memosOn"){
+    this.parentElement.children[1].className = "fixedOn";
+  }
+  if(memoClass == "fixedOn"){
+    this.parentElement.children[1].className = "memosOn";
+  }
+  turnComment(this);
+}
+
+function turnComment(self){
+  var comment = document.getElementById("comment");
+  
+  //No pin picked
+  if(currPin == -1){
+    //Change current Pin
+    currPin = self;
+    //Load current Pin comment
+    comment.value = findMemo(self.id).comment;
+  }
+  //New pin is picked
+  else if(currPin != self){
+    //Save current Pin comment
+    findMemo(currPin.id).comment = comment.value;
+    //Change current Pin
+    currPin.parentElement.children[1].className = "memosOff";
+    currPin = self;
+    //Load current Pin comment
+    comment.value = findMemo(self.id).comment;
+  }
+  //Same pin picked -> unpick pin
+  else{
+    //Save current Pin comment
+    findMemo(currPin.id).comment = comment.value;
+    //Unpick pin
+    currPin = -1;
+  }
+}
+
+function myMove1() {
+  var elem1 = document.getElementById(yearStart + 0);
+  var elem2 = document.getElementById(yearStart + 1);
+  var elem3 = document.getElementById(yearStart + 2);
+  var elem4 = document.getElementById(yearStart + 3);
+  var elem5 = document.getElementById(yearStart + 4);
+  var pos = 0;
+  var limit = pos + 250;
+  var id = setInterval(function(){
+    if (pos >= limit) {
+      clearInterval(id);
+    } else {
+      pos += 10;
+      elem1.style.left = pos + "px";
+      elem2.style.left = pos + "px"; 
+      elem3.style.left = pos + "px"; 
+      elem4.style.left = pos + "px"; 
+      elem5.style.left = pos + "px"; 
+    }
+  }, 10);
+}
+
+function myMove2() {
+  var elem1 = document.getElementById(yearStart + 0);
+  var elem2 = document.getElementById(yearStart + 1);
+  var elem3 = document.getElementById(yearStart + 2);
+  var elem4 = document.getElementById(yearStart + 3);
+  var elem5 = document.getElementById(yearStart + 4);
+  var pos = 250;
+  var limit = pos - 250;
+  var id = setInterval(function(){
+    if (pos >= limit) {
+      clearInterval(id);
+    } else {
+      pos -= 10;
+      elem1.style.left = pos + "px";
+      elem2.style.left = pos + "px"; 
+      elem3.style.left = pos + "px"; 
+      elem4.style.left = pos + "px"; 
+      elem5.style.left = pos + "px"; 
+    }
+  }, 10);
 }
 
 function moveLeft(){
-  console.log("hi");
+  yearStart -= 1;
+  var chart = document.getElementById("life-chart");
+  //myMove1();
+  //If current pin exists, maintain current pin
+  if(currPin != -1){
+    framePinName = currPin.id;
+  }else{
+    framePinName = -1;
+  }
+  
+  while (chart.hasChildNodes()){
+    chart.removeChild(chart.firstChild);
+  }
+  initialize();
+  
+  if(framePinName != -1){
+    var maintainPin = document.getElementById(framePinName);
+    if(maintainPin != null){
+      currPin = maintainPin;
+      currPin.parentElement.children[1].className = "fixedOn";
+    }
+  }
 }
 
 function moveRight(){
-  console.log("hello");
+  var commentDiv = document.getElementById("commentDiv");
+  
+  if(yearStart != 2016){
+    yearStart += 1;
+    var chart = document.getElementById("life-chart");
+  
+    //myMove2();
+    if(currPin != -1){
+      framePinName = currPin.id;
+    }else{
+      framePinName = -1;
+    }
+    
+    while (chart.hasChildNodes()){
+      chart.removeChild(chart.firstChild);
+    }
+    initialize();
+    
+    if(framePinName != -1){
+      var maintainPin = document.getElementById(framePinName);
+      
+      if(maintainPin != null){
+        currPin = maintainPin;
+        currPin.parentElement.children[1].className = "fixedOn";
+      }
+    }
+  }
+}
+
+function deleteButtonShow(){
+  document.getElementById("deleteButton").style.visibility = "visible";
+}
+
+function deleteButtonHide(){
+  document.getElementById("deleteButton").style.visibility = "hidden";
+}
+
+function deleteComment(self){
+  var comment = document.getElementById("comment").value = "";
+  
+  findMemo(currPin.id).comment = comment.value;
 }
 
 function goUploadQuestion(){
@@ -125,6 +312,7 @@ function confirm(){
   // 기존 페이지를 새로운 페이지로 변경
   location.replace("https://jas03006.github.io/525/Building_Stroy/Building_Story.html");
 }
+
 initialize();
 
 
