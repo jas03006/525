b4a374e906dc7c201064386844bad3f1ec577e94
dbxkr97 <dbxkr97@kaist.ac.kr>
2019-05-09 14:52:01 +0900

    project/draft

--- Project/Project.js
+++ Project/Project.js
@@ -70,38 +70,35 @@ function makep(i){
     projectWrapper.addEventListener("mouseleave", function () {
       //leave
       console.log("leave\t"+ projectWrapper.id);
-      projectImage1.style.display = 'block';
-      projectImage2.style.display = 'none';
-      projectOptions.style.display = 'none';    
+      projectOptions1.style.display = 'block';    
+      projectOptions2.style.display = 'none';    
     });
     projectWrapper.addEventListener("mouseenter", function () {
       //enter
       console.log("enter\t"+ projectWrapper.id);
-      projectImage1.style.display = 'none';
-      projectImage2.style.display = 'block';    
-      projectOptions.style.display = 'block';    
+      projectOptions1.style.display = 'none';    
+      projectOptions2.style.display = 'block';    
     });
     var projectImage =document.createElement("div");
     projectWrapper.appendChild(projectImage);
 
     var projectImage1 = document.createElement("div");
-    projectImage1.className = "project_image1";
-    projectImage1.id = projectWrapper.id+"_image1";
+    projectImage1.className = "project_image";
+    projectImage1.id = projectWrapper.id+"_image";
     projectImage1.innerHTML = '<img src="./src/image/project/empty_project.png" width="200" border="0">';
     projectImage.appendChild(projectImage1);
 
-    var projectImage2 = document.createElement("div");
-    projectImage2.className = "project_image2";
-    projectImage2.id = projectWrapper.id+"_image2";
-    projectImage2.innerHTML = '<img src="./src/image/project/empty_project.png" width="200" border="0">';
-    projectImage2.style.display = 'none';
-    projectImage.appendChild(projectImage2);
+    var projectOptions1 = document.createElement("div");
+    projectOptions1.className = "project_options";
+    projectOptions1.innerHTML = projectTitles[i] + "<br>" + projectDates[i];
+    projectOptions1.style.display = 'block';  
+    projectImage.appendChild(projectOptions1);
 
-    var projectOptions = document.createElement("div");
-    projectOptions.className = "project_options";
-    projectOptions.innerHTML = '<a href="./Add_Questions/Add_Questions.html">EDIT PROJECT</a><br><a href="./Draft/Draft.html">WRITE DRAFT</a><br><a href="#">DELETE PROJECT</a>';
-    projectOptions.style.display = 'none';  
-    projectImage.appendChild(projectOptions);
+    var projectOptions2 = document.createElement("div");
+    projectOptions2.className = "project_options";
+    projectOptions2.innerHTML = '<a href="./Add_Questions/Add_Questions.html">EDIT PROJECT</a><br><a href="./Draft/Draft.html">WRITE DRAFT</a><br><a href="#">DELETE PROJECT</a>';
+    projectOptions2.style.display = 'none';  
+    projectImage.appendChild(projectOptions2);
 
     document.getElementById("projects").appendChild(projectWrapper);  
 
